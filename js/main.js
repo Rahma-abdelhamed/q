1e4a8351327233cdebc740a64824d742548bba9e
rahma <113859044+Rahma-abdelhamed@users.noreply.github.com>
2024-04-27 01:02:41 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+
+
+function toggleSize(image) {
+    image.classList.toggle("expanded");
+}
+
+   
+
+
+
+
+
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.image-container img');
+
+function moveSlide(n) {
+    currentSlide += n;
+    showSlide();
+}
+
+function showSlide() {
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    } else if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    
+    slides.forEach((slide, index) => {
+        if (index === currentSlide) {
+            slide.classList.add('expanded');
+        } else {
+            slide.classList.remove('expanded');
+        }
+    });
+}
+
+function toggleSize(image) {
+    image.classList.toggle("expanded");
+}
